72a11e544c23032b0b4c674d1911e1b9d91342b2
ajdavis16@hotmail.com <ajdavis16@hotmail.com>
2017-09-29 12:47:34 -0400

    1st commit

---
+++ react-flashcards/src/Config/Firebase/db_config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    	apiKey: "AIzaSyCM90nziilyja_ZpVtBWI4QJU3qBSjF0ZU",
+    	authDomain: "flashcards-d49bc.firebaseapp.com",
+    	databaseURL: "https://flashcards-d49bc.firebaseio.com",
+    	projectId: "flashcards-d49bc",
+    	storageBucket: "flashcards-d49bc.appspot.com",
+    	messagingSenderId: "871331550088"
+}
